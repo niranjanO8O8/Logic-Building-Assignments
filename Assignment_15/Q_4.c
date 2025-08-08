2a2bc4161e442c96bba9295c48684c8a5678e887
niranjanO8O8 <niranjankhaire0808@gmail.com>
2025-08-08 19:22:31 +0530

    Add files via upload

---
+++ Assignment_15/Q_4.c
@@ -0,0 +1,46 @@
+//*       *       *       *       *
+//*       @       @       @       *
+//*       @       @       @       *
+//*       @       @       @       *
+//*       @       @       @       *
+//*       *       *       *       *
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int iCnt1 = 0;
+    int iCnt2 = 0;
+
+    for(iCnt1 = 1; iCnt1 <= iRow; iCnt1++)
+    {
+        for(iCnt2 = 1; iCnt2 <= iCol; iCnt2++)
+        {
+            if((iCnt1 == 1) || (iCnt1 == iRow) || (iCnt2 == 1) || (iCnt2 == iCol))
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf("@\t");
+            }
+        }
+        printf("\n");
+    }
+
+}
+
+int main()
+{
+    int iValue1 = 0,iValue2 = 0;
+
+    printf("Enter Number of Rows : ");
+    scanf("%d",&iValue1);
+
+    printf("Enter Number of Columns : ");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
