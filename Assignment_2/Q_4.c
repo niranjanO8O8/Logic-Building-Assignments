3412e1af724e130334e729ee7bb1270ee31fe396
niranjanO8O8 <niranjankhaire0808@gmail.com>
2025-08-08 18:57:50 +0530

    Add files via upload

---
+++ Assignment_2/Q_4.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+void Display(int iNo, int iFrequency)
+{
+    int iCnt = 0;
+
+    if(iFrequency < 0)
+    {
+        iFrequency = -iFrequency;
+    }
+
+    for(iCnt = 1;iCnt <= iFrequency; iCnt++)
+    {
+        printf("%d\n",iNo);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    int iCount = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    printf("Enter Frequency : ");
+    scanf("%d",&iCount);
+
+    Display(iValue,iCount);
+
+    return 0;
+}
